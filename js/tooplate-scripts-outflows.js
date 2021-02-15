062105f3e4cb7d4c86c36fbcef31b556fb142d2b
Soheeb <soheeb@gmail.com>
2021-02-15 18:04:44 -0500

    getting to end of inflows set-up

--- js/tooplate-scripts-outflows.js
+++ js/tooplate-scripts-outflows.js
@@ -185,50 +185,9 @@ function updateBarChart() {
   }
 }
 
+// Reload the page so that charts will display correctly
 function reloadPage() {
   setTimeout(function() {
     window.location.reload();
-  }); // Reload the page so that charts will display correctly
-}
-
-function drawCalendar() {
-  if ($("#calendar").length) {
-    $("#calendar").fullCalendar({
-      height: 400,
-      events: [
-        {
-          title: "Meeting",
-          start: "2018-09-1",
-          end: "2018-09-2"
-        },
-        {
-          title: "Marketing trip",
-          start: "2018-09-6",
-          end: "2018-09-8"
-        },
-        {
-          title: "Follow up",
-          start: "2018-10-12"
-        },
-        {
-          title: "Team",
-          start: "2018-10-17"
-        },
-        {
-          title: "Company Trip",
-          start: "2018-10-25",
-		  end: "2018-10-27"
-        },
-        {
-          title: "Review",
-          start: "2018-11-12"
-        },
-        {
-          title: "Plan",
-          start: "2018-11-18"
-        }
-      ],
-      eventColor: "rgba(54, 162, 235, 0.4)"
-    });
-  }
+  }); 
 }
