03b294e79bd594c567690f52dc7901b9715498a1
Soheeb <soheeb@gmail.com>
2021-02-15 23:29:07 -0500

    got year selection working

--- js/inflows_search.js
+++ js/inflows_search.js
@@ -1,13 +1,21 @@
+// Grab Selected Year
+function getYear() {
+    return selectedYear = document.getElementById('year').value;
+}
 
- 
- // Code to grab search button and input
+ // Grab search button and input
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 
+
+// Generate results
 searchButton.addEventListener('click', () => {
 const input = searchInput.value; // grabs what is searched
-html = '<li class="tm-list-group-item"> ' + input + ' </li>'
-document.querySelector("#search").innerHTML = html;
+html = "";
+for (i = 0; i < 5; i++) {
+html += '<li class="tm-list-group-item"> ' + getYear() + ' </li>';
+} 
+document.querySelector("#search").innerHTML = html
 });
 
 
