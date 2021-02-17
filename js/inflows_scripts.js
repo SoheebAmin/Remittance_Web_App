02c76bbbfdd9c5f3c3a1bf066c6befc0c0da69db
Soheeb <soheeb@gmail.com>
2021-02-16 21:13:08 -0500

    shoiwng dollar values correctly

--- js/inflows_scripts.js
+++ js/inflows_scripts.js
@@ -1,3 +1,14 @@
+// Populate Remittance Totals per country
+var country_name = ""
+for (let i = 0; i != inflows_data.length; i++) {
+    current_country = Object.values(inflows_data[i])
+    for (let j = 0; j != current_country.length; j++) {
+        if(typeof current_country[j] === 'string') {
+
+        }
+    }
+}
+
 
 // Grab Selected Year
 function getYear() {
@@ -9,8 +20,11 @@ var searchButton = document.getElementById('search-button');
 var searchInput = document.getElementById('search-input');
 
 
-// Generate results
+// Get Remittance Results for Country, per given year
 function searchCountry() {
+    // for time measurement
+    var start = window.performance.now();
+
     const input = searchInput.value; 
     var country = "";
     // Checks to see if the searched country is actually a country in our database 
@@ -49,13 +63,18 @@ function searchCountry() {
         remittance_data = "No data on record for this year";
     }
     else {
-        remittance_data = "$ " + remittance_data + " Thousand USD"};
-    
+        remittance_data = remittance_data * 1000000; // multiply by 1000 because data is in millions
+        remittance_data = remittance_data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); // add thousands separators
+        remittance_data = "$" + remittance_data + " USD"}; 
+
+    var end = window.performance.now();
+    var time = end - start;   
+
     html = '<li class="tm-list-group-item"> ' + remittance_data + ' </li>';
+    html += '<li class="tm-list-group-item">  Query Time: ' + time + ' ms </li>';
     document.querySelector("#search").innerHTML = html
+    
+ 
 };
-
-
-// // JSON Testing
 
 
